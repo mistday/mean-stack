7bbf43dfb1ecb05e5e19f7c0ca344503a6e792aa
Dmitry <mistday@yandex.ru>
2015-03-10 15:17:11 +0300

    little change

--- demo.js
+++
@@ -1,3 +0,0 @@
-module.exports = mongoose.model('onecol', {
-	name: String
-});
--- server/controllers/demo-controller.js
+++
@@ -1,8 +0,0 @@
-var Demo = require('../demo.js'); 
-
-module.exports.create = function(req, res) {
-  var demo = new Demo(req.body);
-  demo.save(function(err, result) {
-  	res.json(result);
-  });
-}
